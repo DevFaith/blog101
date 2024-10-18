1799f3939afa7de56e9038140e8d9f02ef08350c
DevFaith <waitherafaith@outlook.com>
2024-10-18 23:12:55 +0300

    * feat(App.jsx): add App component with routes and components for Header, Footer, Home, Blogs, Addblog, and Login

---
+++ Frontend/blog/src/App.jsx
@@ -0,0 +1,33 @@
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Home from './Pages/Home'
+import Blogs from './Pages/Blogs'
+import Addblog from './Pages/Addblog'
+import Login from './Pages/Login'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+
+  return (
+    < >
+      <BrowserRouter>
+      <div className='z-10'>
+      <Header />
+      </div>
+      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/addblog' element={<Addblog />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+      
+    </>
+    
+  )
+}
+
+export default App
